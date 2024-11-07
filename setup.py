6d49c05e42de40a6e1d7800cc44783d92546ea77
RawatRahul14 <rawatrahul1604@gmail.com>
2024-11-07 20:03:01 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version = "0.0.0",
+    author = "rawatrahul14",
+    author_email = "rahulrawat272chd@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
